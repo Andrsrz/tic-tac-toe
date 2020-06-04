efd1bf252a12638b5d7a7c10d57db5a1d35dc23e
Andres Ruiz <andrsruiz@gmail.com>
2020-06-04 18:34:36 -0500

    Update Game Logic

--- js/gameboard.js
+++ js/gameboard.js
@@ -1,14 +1,28 @@
-const Gameboard = ((cells) => {
+const Gameboard = ((cells, removedCells) => {
+	var P1;
+	var P2OrCPU;
 	/* This method updates the available cells of the
 	 * gameboard, deleting the last cell that have been
 	 * played. */
 	const updateAvailableCells = (cellToRemove) => {
-		cells.splice(cellToRemove, 1);
+		/* We store our cells in two arrays to have more
+		 * control over out data. */
+		removedCells.push(Number(cellToRemove));
+		cells.splice(Number(cellToRemove), 1);
+		console.log("cells available : " + cells);
+		console.log("cells not available : " + removedCells);
+	}
+
+	const resetCells = () => {
+		cells = [0,1,2,3,4,5,6,7,8];
+		removedCells = [];
 	}
 
 	const clearBoard = () => {
+		resetCells();
 		for(let i = 0; i < cells.length; i++){
 			let cell = document.getElementById(i);
+			cell.setAttribute("class", "tic-tac-toe-cell");
 			/* This actually clears the cell */
 			while(cell.firstChild){
 				cell.removeChild(cell.firstChild);
@@ -20,29 +34,20 @@ const Gameboard = ((cells) => {
 		console.log("p1vscpu");
 		reset();
 		/* The players */
-		let player = Player("P1", [], "X");
-		let cpu = Player("CPU", [], "O");
-		while(cells.length >= 0){
-			if(cells.length % 2 == 0){
-				/* Pick a cell */
-				/* Add to player array */
-				/* Check for winning */
-				/* Update cells available */
-			}else{
-				/* Pick a cell */
-				/* Add to player array */
-				/* Check for winning */
-				/* Update cells available */
-			}
-		}
+		P1 = Player("P1", [], "X");
+		P2OrCPU = Player("CPU", [], "O");
 	}
 
 	const p1vsp2 = () => {
 		console.log("p1vsp2");
 		reset();
+		/* The players */
+		P1 = Player("P1", [], "X");
+		P2OrCPU = Player("P2", [], "O");
 	}
 
 	const start = (mode) => {
+		reset();
 		if(mode === 0){
 			p1vscpu();
 		}else if(mode === 1){
@@ -55,13 +60,69 @@ const Gameboard = ((cells) => {
 		clearBoard();
 	}
 
-	const turn = (element) => {
-		console.log("turn " + element.target.id);
+	const display = (text, cell) => {
 		let h2 = document.createElement("h2");
+		h2.innerHTML = text;
 		h2.style.alignSelf = "center";
-		h2.innerHTML = "TEST";
-		element.target.appendChild(h2);
+		cell.appendChild(h2);
+	}
+
+	const checkForWinning = (player) => {
+		const winningCombinations = [ [0,1,2],
+									  [3,4,5],
+									  [6,7,8],
+									  [0,3,6],
+									  [1,4,7],
+									  [2,5,8],
+									  [0,4,8],
+									  [2,4,6] ];
+		const playerMoves = player.getMovements();
+		for(let i = 0; i < winningCombinations.length; i++){
+			if(playerMoves.includes(winningCombinations[i][0]) &&
+				playerMoves.includes(winningCombinations[i][1]) &&
+				playerMoves.includes(winningCombinations[i][2])){
+				return true;
+			}else{
+				return false;
+			}
+		}
+	}
+
+	const turn = (element) => {
+		/* Disable cell to the user */
+		const cell = element.target;
+		cell.setAttribute("class", "tic-tac-toe-cell cell-disabled");
+		/* Update cells */
+		updateAvailableCells(cell.id);
+		/* game */
+		if(cells.length % 2 == 0){
+			/* P1 Stars */
+			P1.getMovements().push(cell.id);
+			display(P1.getWeapon(), cell);
+			/* Check winning */
+			if(checkForWinning(P1)){
+				endGame(P1);
+			}
+		}else if(cells.length % 2 != 0){
+			P2OrCPU.getMovements().push(cell.id);
+			display(P2OrCPU.getWeapon(), cell);
+			/* Check winning */
+			if(checkForWinning(P2OrCPU)){
+				endGame(P2OrCPU);
+			}
+		}else{
+			endGame(null);
+		}
+	}
+
+	const endGame = (winner) => {
+		if(winner === null){
+			console.log("A Tie");
+		}else{
+			console.log(winner + " Wins");
+		}
+		reset();
 	}
 
 	return {start, reset, turn};
-})([0,1,2,3,4,5,6,7,8]);
+})([0,1,2,3,4,5,6,7,8], []);
