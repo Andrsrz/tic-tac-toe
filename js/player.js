90d4be3a2cc80f38b1b8eb8a20c3115010826274
Andres Ruiz <andrsruiz@gmail.com>
2020-06-04 12:45:13 -0500

    Add Player Class

---
+++ js/player.js
@@ -0,0 +1,24 @@
+const Player = (name, movements, weapon) => {
+	const getName = () => name;
+	const getMovements = () => movements;
+	const getWeapon = () => weapon;
+
+	/* In order to  update the player movements
+	 * I need the cell's number and add it to the
+	 * movements array */
+	const updateMovements = (cell) => {
+		let moves = getMovements();
+		moves.push(cell);
+	}
+
+	/* The attack methos works as follows:
+	 * we get the cell's number the player is going
+	 * to attack to update the player movements and
+	 * put the player's weapon. */
+	const attack = (cell) => {
+		updateMovements(cell);
+		return getWeapon();
+	}
+
+	return {getName, getMovements, getWeapon, attack};
+};
